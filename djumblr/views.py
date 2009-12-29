b968a6fe08e443355eb430aeff056454ac67b460
Jeff Triplett <jeff.triplett@gmail.com>
2009-12-29 02:20:01 -0600

    initial frontend for adding content

--- djumblr/views.py
+++ djumblr/views.py
@@ -1,5 +1,12 @@
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
+from django.shortcuts import render_to_response
 from django.views.generic import date_based, list_detail
+from django.template import RequestContext
+
 from djumblr.models import TumbleItem
+from djumblr.models import Audio, Conversation, Link, Photo, Quote, Regular, Video
+from djumblr.forms import AudioForm, ConversationForm, LinkForm, PhotoForm, QuoteForm, RegularForm, VideoForm
 
 
 def tumble_object_list(request, page=0, **kwargs):
@@ -73,3 +80,79 @@ def tumble_item_detail(request, year, month, day, tumblr_id, template_name='djum
         template_name = template_name,
         **kwargs
     )
+
+
+@login_required
+def tumble_item_form(request):
+    audio_form = AudioForm()
+    conversation_form = ConversationForm()
+    link_form = LinkForm()
+    photo_form = PhotoForm()
+    quote_form = QuoteForm()
+    regular_form = RegularForm()
+    video_form = VideoForm()
+
+    if request.method == 'POST':
+        new_data = request.POST.copy()
+        our_forms = ('audio-form', 'conversation-form', 'link-form', 'photo-form', 'quote-form', 'regular-form', 'video-form')
+        delete_forms = tuple('delete-%s' % x for x in our_forms)
+
+        # Add forms
+        if any(x in new_data for x in our_forms):
+            if new_data.get('audio-form'):
+                form = AudioForm(new_data)
+                audio_form = form
+            elif new_data.get('conversation-form'):
+                form = form
+                conversation_form = ConversationForm()
+            elif new_data.get('link-form'):
+                form = LinkForm(new_data)
+                link_form = form
+            elif new_data.get('photo-form'):
+                form = PhotoForm(new_data)
+                photo_form = form
+            elif new_data.get('quote-form'):
+                form = QuoteForm(new_data)
+                quote_form = form
+            elif new_data.get('regular-form'):
+                form = RegularForm(new_data)
+                regular_form = form
+            elif new_data.get('video-form'):
+                form = VideoForm(new_data)
+                video_form = form
+
+            if form.is_valid():
+                post = form.save(commit=False)
+                post.user = request.user
+                post.save()
+                return HttpResponseRedirect(request.path)
+
+        # Delete forms
+        elif any(x in new_data for x in delete_forms):
+            delete_id = request.POST['delete_id']
+            if new_data.get('delete-audio-form'):
+                Audio.objects.get(id=delete_id).delete()
+            elif new_data.get('delete-conversation-form'):
+                Conversation.objects.get(id=delete_id).delete()
+            elif new_data.get('delete-link-form'):
+                Link.objects.get(id=delete_id).delete()
+            elif new_data.get('delete-photo-form'):
+                Photo.objects.get(id=delete_id).delete()
+            elif new_data.get('delete-quote-form'):
+                Quote.objects.get(id=delete_id).delete()
+            elif new_data.get('delete-regular-form'):
+                Regular.objects.get(id=delete_id).delete()
+            elif new_data.get('delete-video-form'):
+                Video.objects.get(id=delete_id).delete()
+
+            return HttpResponseRedirect(request.path)
+
+    return render_to_response('djumblr/tumbleitem_form.html', {
+        'audio_form': audio_form,
+        'conversation_form': conversation_form,
+        'link_form': link_form,
+        'photo_form': photo_form,
+        'quote_form': quote_form,
+        'regular_form': regular_form,
+        'video_form': video_form,
+    }, context_instance=RequestContext(request))
