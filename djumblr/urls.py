0bed9c034b65821482a82d88558e45d8b6215e5a
jefftriplett <jeff.triplett@gmail.com>
2010-07-14 00:36:26 -0500

    cleaned up url ordering

--- djumblr/urls.py
+++ djumblr/urls.py
@@ -3,66 +3,73 @@ from djumblr import views
 
 
 urlpatterns = patterns('',
-    url(r'^$',
-        views.tumble_archive_index,
-        name='djumblr_archive_index'),
-    url(r'^(?P<year>\d{4})/$',
-        views.tumble_archive_year,
-        name='djumblr_archive_year'),
-    url(r'^(?P<year>\d{4})/(?P<month>\w{3})/$',
-        views.tumble_archive_month,
-        name='djumblr_archive_month'),
-    url(r'^(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{2})/$',
-        views.tumble_archive_day,
-        name='djumblr_archive_day'),
-    url(r'^(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{2})/(?P<tumblr_id>[-\w]+)/$',
+    url(r'^(?P<content_type>[-\w]+)/(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{2})/(?P<tumblr_id>[-\w]+)/$',
         views.tumble_archive_object_detail,
-        name='djumblr_detail'),
-    url(r'^list/$',
-        views.tumble_object_list,
-        name='djumblr_object_list'),
-    url(r'edit/',
-        views.tumble_item_form,
-        name='tumble_form'),
+        name='djumblr_content_type_detail'),
 
-    # tag related
-    url(r'^tags/(?P<slug>(.*))/$',
-        views.tumble_tag_detail,
-        name='djumblr_tag_detail',
-    ),
-    url(r'^tags/$',
-        views.tumble_tag_list,
-        name='djumblr_tag_list'
-    ),
+    url(r'^(?P<content_type>[-\w]+)/(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{2})/$',
+        views.tumble_archive_day,
+        name='djumblr_content_type_archive_day'),
 
-    # urls based on the content_type of the TumbleItem...
-    url(r'^(?P<content_type>[-\w]+)/$',
-        views.tumble_archive_index,
-        name='djumblr_content_type_archive_index'),
-    url(r'^(?P<content_type>[-\w]+)/list/$',
-        views.tumble_object_list,
-        name='djumblr_content_type_object_list'),
+    url(r'^(?P<content_type>[-\w]+)/(?P<year>\d{4})/(?P<month>\w{3})/$',
+        views.tumble_archive_month,
+        name='djumblr_content_type_archive_month'),
 
-    # tag + content_type related
+    url(r'^(?P<content_type>[-\w]+)/(?P<year>\d{4})/$',
+        views.tumble_archive_year,
+        name='djumblr_content_type_archive_year'),
+
     url(r'^(?P<content_type>[-\w]+)/tags/(?P<slug>(.*))/$',
         views.tumble_tag_detail,
         name='djumblr_tag_detail',
     ),
+
     url(r'^(?P<content_type>[-\w]+)/tags/$',
         views.tumble_tag_list,
         name='djumblr_tag_list'
     ),
 
-    url(r'^(?P<content_type>[-\w]+)/(?P<year>\d{4})/$',
-        views.tumble_archive_year,
-        name='djumblr_content_type_archive_year'),
-    url(r'^(?P<content_type>[-\w]+)/(?P<year>\d{4})/(?P<month>\w{3})/$',
-        views.tumble_archive_month,
-        name='djumblr_content_type_archive_month'),
-    url(r'^(?P<content_type>[-\w]+)/(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{2})/$',
-        views.tumble_archive_day,
-        name='djumblr_content_type_archive_day'),
-    url(r'^(?P<content_type>[-\w]+)/(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{2})/(?P<tumblr_id>[-\w]+)/$',
+    url(r'^(?P<content_type>[-\w]+)/archive/$',
+        views.tumble_archive_index,
+        name='djumblr_content_type_archive_index'),
+
+    url(r'^(?P<content_type>[-\w]+)/$',
+        views.tumble_object_list,
+        name='djumblr_content_type_object_list'),
+
+    url(r'^(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{2})/(?P<tumblr_id>[-\w]+)/$',
         views.tumble_archive_object_detail,
-        name='djumblr_content_type_detail'),
+        name='djumblr_detail'),
+
+    url(r'^(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{2})/$',
+        views.tumble_archive_day,
+        name='djumblr_archive_day'),
+
+    url(r'^(?P<year>\d{4})/(?P<month>\w{3})/$',
+        views.tumble_archive_month,
+        name='djumblr_archive_month'),
+
+    url(r'^(?P<year>\d{4})/$',
+        views.tumble_archive_year,
+        name='djumblr_archive_year'),
+
+    url(r'^tags/(?P<slug>(.*))/$',
+        views.tumble_tag_detail,
+        name='djumblr_tag_detail'),
+
+    url(r'^tags/$',
+        views.tumble_tag_list,
+        name='djumblr_tag_list'),
+
+    url(r'^archive/$',
+        views.tumble_archive_index,
+        name='djumblr_archive_index'),
+
+    url(r'edit/$',
+        views.tumble_item_form,
+        name='tumble_form'),
+
+    url(r'^$',
+        views.tumble_object_list,
+        name='djumblr_object_list'),
 )
